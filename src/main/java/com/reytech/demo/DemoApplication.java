11c248fe44862a913d55a216b63496349154506f
StuAalto <68701478+StuAalto@users.noreply.github.com>
2020-09-23 09:17:18 +0200

    Update DemoApplication.java
    private String toto

--- src/main/java/com/reytech/demo/DemoApplication.java
+++ src/main/java/com/reytech/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+private String name="toto";
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
